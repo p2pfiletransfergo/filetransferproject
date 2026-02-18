a7a71db035f1bb5f4b98b67bf0688429564b2762
Bharathjangid <bharathjangid04@gmail.com>
2026-02-18 23:19:02 +0530

    Fixed sendfilego

--- main.go
+++ main.go
@@ -40,5 +40,5 @@ func main() {
 		}
 
 	}
-
+	select {}
 }
--- sendfile.go
+++ sendfile.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/binary"
 	"io"
 	"net"
 	"os"
@@ -29,6 +30,14 @@ func sendtcp(addr string, path string) {
 	defer file.Close()
 	p := file.Name()
 	p = filepath.Base(p)
+
+	binary.Write(conn, binary.BigEndian, uint32(len(p)))
 	conn.Write([]byte(p))
+	fileinfo, _ := file.Stat()
+	filesize := fileinfo.Size()
+	binary.Write(conn, binary.BigEndian, filesize)
 	io.Copy(conn, file)
+
+	// conn.Write([]byte(p))
+	// io.Copy(conn, file)
 }
